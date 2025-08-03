6f588baa4c2662dd02e58b8b2b48662ab0aa604f
Sushil Sharma <sharma.s420823@gmail.com>
2025-08-03 23:51:04 +0545

    added_feature_class

--- scraper.py
+++ scraper.py
@@ -1,3 +1,37 @@
+# # # from selenium import webdriver
+# # # from selenium.webdriver.chrome.service import Service
+# # # from selenium.webdriver.common.by import By
+# # # import time
+
+# # # service = Service(executable_path="chromedriver.exe")
+# # # driver = webdriver.Chrome(service=service)
+
+# # # driver.get("https://weworkremotely.com/")
+# # # time.sleep(10)  # give time for the page to load
+
+# # # # find all job listings
+# # # job_listings = driver.find_elements(By.CLASS_NAME, "new-listing-container")
+# # # print(f"Found {len(job_listings)} jobs")
+
+# # # # extract links or titles
+# # # for job in job_listings:
+# # #     try:
+# # #         link = job.find_element(By.TAG_NAME, "a").get_attribute("href")
+# # #         company_name=job.find_element(By.CLASS_NAME,"new-listing__categories")
+# # #         # print(company_name)
+# # #         if(len(company_name)>=3):
+# # #             work_time=company_name[0].text
+# # #             salary=company_name[1].text
+# # #             location=company_name[2].text
+# # #             print(f"{link}-{work_time}-{salary}-{location}")
+# # #         # print(link)
+# # #     except:
+# # #         continue
+
+# # # driver.quit()
+
+
+
 # # from selenium import webdriver
 # # from selenium.webdriver.chrome.service import Service
 # # from selenium.webdriver.common.by import By
@@ -7,61 +41,27 @@
 # # driver = webdriver.Chrome(service=service)
 
 # # driver.get("https://weworkremotely.com/")
-# # time.sleep(10)  # give time for the page to load
+# # time.sleep(10)  # you don't need 60 seconds
 
-# # # find all job listings
 # # job_listings = driver.find_elements(By.CLASS_NAME, "new-listing-container")
 # # print(f"Found {len(job_listings)} jobs")
 
-# # # extract links or titles
 # # for job in job_listings:
 # #     try:
+# #         title=job.find_element(By.CLASS_NAME,"new-listing__header__title")
+# #         company=job.find_element(By.CLASS_NAME,"new-listing__company-name")
 # #         link = job.find_element(By.TAG_NAME, "a").get_attribute("href")
-# #         company_name=job.find_element(By.CLASS_NAME,"new-listing__categories")
-# #         # print(company_name)
-# #         if(len(company_name)>=3):
-# #             work_time=company_name[0].text
-# #             salary=company_name[1].text
-# #             location=company_name[2].text
-# #             print(f"{link}-{work_time}-{salary}-{location}")
-# #         # print(link)
-# #     except:
+# #         categories = job.find_elements(By.CLASS_NAME, "new-listing__categories__category")
+# #         if len(categories) >= 3:
+# #             work_time = categories[0].text
+# #             salary = categories[1].text
+# #             location = categories[2].text
+# #             print(f"{link} - Title:{title.text} - Time:{work_time} - Name:{company.text} - Salary:{salary} - Location: {location}")
+        
+# #     except Exception as e:
 # #         continue
 
 # # driver.quit()
-
-
-
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# import time
-
-# service = Service(executable_path="chromedriver.exe")
-# driver = webdriver.Chrome(service=service)
-
-# driver.get("https://weworkremotely.com/")
-# time.sleep(10)  # you don't need 60 seconds
-
-# job_listings = driver.find_elements(By.CLASS_NAME, "new-listing-container")
-# print(f"Found {len(job_listings)} jobs")
-
-# for job in job_listings:
-#     try:
-#         title=job.find_element(By.CLASS_NAME,"new-listing__header__title")
-#         company=job.find_element(By.CLASS_NAME,"new-listing__company-name")
-#         link = job.find_element(By.TAG_NAME, "a").get_attribute("href")
-#         categories = job.find_elements(By.CLASS_NAME, "new-listing__categories__category")
-#         if len(categories) >= 3:
-#             work_time = categories[0].text
-#             salary = categories[1].text
-#             location = categories[2].text
-#             print(f"{link} - Title:{title.text} - Time:{work_time} - Name:{company.text} - Salary:{salary} - Location: {location}")
-        
-#     except Exception as e:
-#         continue
-
-# driver.quit()
 
 
 
@@ -107,27 +107,55 @@ def extract_jobs():
     return jobs
 
 all_jobs = extract_jobs()
-print(f"Found {len(all_jobs)} jobs before clicking View All")
-
-try:
-    view_all_button = driver.find_element(By.LINK_TEXT, "View all")
-    actions = ActionChains(driver)
-    actions.move_to_element(view_all_button).perform()  
-    time.sleep(2)
-    view_all_button.click()
-    print("Clicked View All button")
-
-   
-    time.sleep(20)
-    more_jobs = extract_jobs()
-    print(f"Found {len(more_jobs)} jobs after clicking View All")
-
-    all_jobs = more_jobs  
-
-except Exception as e:
-    print("View All button not found or error:", e)
 
 for job in all_jobs:
     print(job)
 
+
+def featured_jobs():
+    f_jobs=[]
+    f_jobs_list = driver.find_elements(By.CSS_SELECTOR, ".new-listing-container.feature")
+    for lists in f_jobs_list:
+        try:
+            title = lists.find_element(By.CLASS_NAME, "new-listing__header__title").text
+            company = lists.find_element(By.CLASS_NAME, "new-listing__company-name").text
+            link = lists.find_element(By.TAG_NAME, "a").get_attribute("href")
+            categories = lists.find_elements(By.CLASS_NAME, "new-listing__categories__category")
+            if len(categories) >= 3:
+                work_time = categories[0].text
+                salary = categories[1].text
+                location = categories[2].text
+                f_jobs.append({
+                    'link': link,
+                    'title': title,
+                    'company': company,
+                    'work_time': work_time,
+                    'salary': salary,
+                    'location': location
+                })
+
+
+        except Exception:
+            continue
+    return f_jobs
+fe_jobs=featured_jobs()
+print(f"Total featured jobs: {fe_jobs}")
+for j in fe_jobs:
+    print(j)
+
+
+
+print(f"Found {len(all_jobs)} jobs before clicking View All")
+print(f"Total featured jobs: {fe_jobs}")
 driver.quit()
+
+import json
+
+# Save all jobs
+with open("all_jobs.json", "w", encoding="utf-8") as f:
+    json.dump(all_jobs, f, ensure_ascii=False, indent=4)
+
+# Save featured jobs
+with open("featured_jobs.json", "w", encoding="utf-8") as f:
+    json.dump(fe_jobs, f, ensure_ascii=False, indent=4)
+
